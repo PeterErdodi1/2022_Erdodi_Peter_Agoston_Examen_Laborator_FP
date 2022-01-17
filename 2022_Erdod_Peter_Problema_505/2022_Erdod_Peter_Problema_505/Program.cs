079fe740c616e73857cd586795f5bd8c4853550d
PeterErdodi1 <79203115+PeterErdodi1@users.noreply.github.com>
2022-01-17 10:53:09 +0100

    Probleme

---
+++ 2022_Erdod_Peter_Problema_505/2022_Erdod_Peter_Problema_505/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2022_Erdod_Peter_Problema_505
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string string1 = Console.ReadLine();
+            string[] v1 = string1.Split(' ');
+            string string2 = Console.ReadLine();
+            string[] v2 = string1.Split(' ');
+            int[] a = Array.ConvertAll(v1, int.Parse);
+            int[] b = Array.ConvertAll(v2, int.Parse);
+            bool ok = true;
+            for (int i = 0; i < n; ++i)
+            {
+                bool ogl = false;
+                for (int j = 0; j < n; ++j)
+                    if (a[i] == b[j] && !ogl)
+                    {
+                        ogl = true;
+                        b[j] = -1;
+                    }
+                if (!ogl)
+                    ok = false;
+            }
+            if (ok)
+            {
+                Console.WriteLine("DA");
+            }
+            else
+            {
+                Console.WriteLine("NU");
+            }
+            
+        }
+    }
+}
---
+++ 2022_Erdodi_Peter_Agoston_Problema_294/Probelma 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Probelma_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n =int.Parse(Console.ReadLine());
+            int[] a= new int [n];
+            for (int i = 0; i < n; i++)
+                a[i]=int.Parse(Console.ReadLine());
+            int ca = a[0];
+            int ogl = 0;
+            while (ca!=0)
+            {
+                ogl = ogl * 10 + ca % 10;
+                ca /= 10;
+            }
+            bool ok = false;
+            for (int i = 1; i < n; i++)
+                if (ogl == a[i])
+                    ok = true;
+            if (ok)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
+        }
+    }
+}
