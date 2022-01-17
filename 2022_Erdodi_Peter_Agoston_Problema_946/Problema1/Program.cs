232d00822cb98c25135fdc1eb9553155e1020e7d
PeterErdodi1 <79203115+PeterErdodi1@users.noreply.github.com>
2022-01-17 11:05:20 +0100

    Probleme

---
+++ 2022_Erdodi_Peter_Agoston_Problema_946/Problema1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problema1
+{
+    class Program
+    {
+
+        static void binary(string n)
+        {
+            string r;
+            int k = Convert.ToInt32(n);
+            r = "";
+            while (k > 3)
+            {
+                int a = k % 4;
+                r = Convert.ToString(a) + r;
+                k /= 4;
+            }
+            r = Convert.ToString(k) + r;
+            Console.WriteLine("Binary: {0}", r);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N=  ");
+            string n = Console.ReadLine();
+            //Console.WriteLine("Convert.ToInt32(base2, 2) = " +
+            Convert.ToInt32(n, 2);
+            binary(n);
+        }
+    
+        
+    }
+}
